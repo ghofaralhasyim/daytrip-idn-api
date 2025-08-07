5d91412bd2b906a2acfa32cd1f715d807928754f
Ghof <ghofar.it@gmail.com>
2025-08-07 07:47:24 +0700

    fix routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,20 +3,18 @@ package routes
 import (
 	"database/sql"
 
-	"github.com/daytrip-idn-api/internal/http"
+	"github.com/daytrip-idn-api/internal/controllers"
 	"github.com/daytrip-idn-api/internal/repositories"
-	"github.com/daytrip-idn-api/internal/services"
+	"github.com/daytrip-idn-api/internal/usecases"
 	"github.com/labstack/echo/v4"
 )
 
 func SetupRoutes(e *echo.Echo, db *sql.DB) {
 	apiv1 := e.Group("/v1")
 
-	apiv1.Static("/public/images/articles", "public/images/articles")
-
 	userRepo := repositories.NewUserRepository(db)
-	userService := services.NewUserService(userRepo)
-	userHandler := http.NewUserHandler(userService)
+	userService := usecases.NewUserService(userRepo)
+	userHandler := controllers.NewUserHandler(userService)
 	apiv1.POST("/login", userHandler.Login)
 
 }
